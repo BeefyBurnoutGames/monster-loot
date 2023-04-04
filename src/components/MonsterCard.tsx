582213c8111f42cc3ceff88d1493073a8e3757d1
StephenSHorton <4stephenhorton@gmail.com>
2023-04-04 17:20:07 -0600

    🎨 style(MonsterCard.tsx): improve MonsterCard component layout The MonsterCard component now accepts two props, src and name, which are destructured from the props object. The image container now has overflow hidden to prevent the image from overflowing the container. The image is now displayed using an img tag instead of a placeholder div. The name is now displayed in the content section of the card.
    🔍 chore(MonsterList.tsx): add pagination to MonsterList
    ✨ feat(MonsterList.tsx): load monsters from PocketBase API
    🐛 fix(MonsterList.tsx): fix useEffect infinite loop
    The MonsterList component now loads monsters from the PocketBase API and displays them using pagination. The useEffect hook has been fixed to prevent an infinite loop.

--- src/components/MonsterCard.tsx
+++ src/components/MonsterCard.tsx
@@ -1,11 +1,17 @@
-export default function MonsterCard() {
+export default function MonsterCard({
+	src,
+	name,
+}: {
+	src: string
+	name: string
+}) {
 	return (
 		<div className="flex space-x-2 select-none group">
-			<div className="h-20 w-20 bg-secondary group-hover:bg-secondary/75 group-active:bg-secondary/50 flex items-center justify-center transition-colors group-hover:duration-0">
-				Image
+			<div className="h-20 w-20 oveflow-hidden bg-secondary group-hover:bg-secondary/75 group-active:bg-secondary/50 flex items-center justify-center transition-colors group-hover:duration-0">
+				<img src={src} className="bg-cover bg-center" alt="monster preview" />
 			</div>
 			<div className="bg-secondary flex items-center flex-1 p-2 group-hover:bg-secondary/75 group-active:bg-secondary/50 transition-colors group-hover:duration-0">
-				Content
+				{name}
 			</div>
 		</div>
 	)
